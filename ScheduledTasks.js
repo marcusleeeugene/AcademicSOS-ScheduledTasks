d68bf77c25f7e049dc6bfddf34d119b7d02d172e
Marcus Lee Eugene <mctjl_1997@yahoo.com.sg>
2020-07-04 01:28:18 +0800

    Revert "Updated check for every booking instead of only those with consultationStatus of Confirmed"
    This reverts commit 4ea695c0531f02d31973db3249c4c872f2da06cb.

--- ScheduledTasks.js
+++ ScheduledTasks.js
@@ -48,16 +48,18 @@ module.exports = {
             for (var userBookings in bookings) { //Loop each booking
               var individualBookings = bookings[userBookings];
               var consultStatus = individualBookings["consultStatus"];
-              var bookingId = Object.keys(bookings)[0];
-              var consultDate = individualBookings["consultDate"];
-              var consultEndTime = individualBookings["consultEndTime"];
-              var currentDateTime = moment(new Date(), ["DD-MMM-YY hh:mm A"]).format();
-              var consultationEndDateTime = moment(consultDate + " " + consultEndTime, ["DD-MMM-YY hh:mm A"]).format();
-              if (currentDateTime >= consultationEndDateTime) { //check if consultation date and time ended
-                console.log("enter 3");
-                completeConsultation(modCode, bookingId, bookings[bookingId]);
+              if (consultStatus != "Pending") { //If consultation is completed
+                //Loop through each booking
+                var bookingId = Object.keys(bookings)[0];
+                var consultDate = individualBookings["consultDate"];
+                var consultEndTime = individualBookings["consultEndTime"];
+                var currentDateTime = moment(new Date(), ["DD-MMM-YY hh:mm A"]).format();
+                var consultationEndDateTime = moment(consultDate + " " + consultEndTime, ["DD-MMM-YY hh:mm A"]).format();
+                if (currentDateTime >= consultationEndDateTime) { //check if consultation date and time ended
+                  console.log("enter 3");
+                  completeConsultation(modCode, bookingId, bookings[bookingId]);
+                }
               }
-
             }
           }
         }
